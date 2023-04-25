c98406dc57bb51a2661fcbd81203d5074ead2d90
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-04-24 21:32:11 -0300

    Atualizando exercícios

--- index.js
+++ index.js
@@ -1,58 +1,40 @@
-function somar(n1, n2){
-    return n1 + n2 
-}
+function calcular(){
+    let operacao = document.getElementsByName('operacao')
+    let numero1 = document.getElementById('numero-1')
+    let numero2 = document.getElementById('numero-2')
+    let resultado = document.getElementById('resultado')
 
-function subtrair(n1, n2){
-    return n1 - n2
-}
+    function somar (numero1, numero2){
+    return numero1 + numero2
+    }
 
-function multiplicar(n1, n2){
-    return n1 * n2
-}
+    function subtrair (numero1, numero2){
+    return numero1 - numero2
+    }
 
-function dividir(n1, n2){
-    return n1 / n2
-}
+    function multiplicar (numero1, numero2){
+    return numero1 * numero2
+    }
 
-let escolha = 0
-while(escolha != 5){
-    escolha = prompt('Digite [1] PARA SOMAR, [2] PARA SUBTRAIR, [3] PARA MULTIPLICAR, [4] PARA DIVIDIR, [5] PARA CANCELAR')
-    let resultado = 0
-        if (escolha == 1){
-            let numero1 = prompt('Digite um número: ')
-            let numero2 = prompt('Digite outro número: ')
-            numero1 = Number(numero1)
-            numero2 = Number(numero2)
-            resultado = somar(numero1, numero2)
-            alert(`O resultado da soma é ${resultado}`)
+    function dividir (numero1, numero2){
+    return numero1 / numero2
+    }
+
+    if(numero1.value.length == 0 || numero2.value.length == 0 || operacao[false]){
+        alert('[ERRO] Você precisa digitar os números e escolher a operação que deseja calcular.')
+    }else{
+        
+        numero1 = Number(numero1.value)
+        numero2 = Number(numero2.value)
+        
+        if(operacao[0].checked){
+            resultado.innerHTML = `O resultado da soma é ${somar(numero1, numero2)}`
+        }else if(operacao[1].checked){
+            resultado.innerHTML = `O resultado da soma é ${subtrair(numero1, numero2)}`
+        }else if(operacao[2].checked){
+            resultado.innerHTML = `O resultado da soma é ${multiplicar(numero1, numero2)}`
+        }else if(operacao[3].checked){
+            resultado.innerHTML = `O resultado da soma é ${dividir(numero1, numero2)}`
         }
-
-        else if(escolha == 2){
-            let numero1 = prompt('Digite um número: ')
-            let numero2 = prompt('Digite outro número: ')
-            numero1 = Number(numero1)
-            numero2 = Number(numero2)
-            resultado = subtrair(numero1, numero2)
-            alert(`O resultado da subtração é ${resultado}`)
-        }
-
-        else if(escolha == 3){
-            let numero1 = prompt('Digite um número: ')
-            let numero2 = prompt('Digite outro número: ')
-            numero1 = Number(numero1)
-            numero2 = Number(numero2)
-            resultado = multiplicar(numero1, numero2)
-            alert(`O resultado da multiplicação é ${resultado}`)
-        }
-
-        else if(escolha == 4){
-            let numero1 = prompt('Digite um número: ')
-            let numero2 = prompt('Digite outro número: ')
-            numero1 = Number(numero1)
-            numero2 = Number(numero2)
-            resultado = dividir(numero1, numero2)
-            alert(`O resultado da divisão é ${resultado}`)
-        }
+    }
 }
-
-
